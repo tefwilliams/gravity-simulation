de70b9fb35c4b6cee803764ec476f70a834d8551
Thomas Williams <tefwilliams@icloud.com>
2021-11-13 17:49:04 +0000

    Update main.py to run n-body problem using numba

--- main.py
+++ main.py
@@ -1,17 +1,75 @@
+import time
+import random
 import numpy as np
-from numba import vectorize
+from numba import njit, prange
+from numpy import sqrt
+
+nParticles = 100000
+
+particle = np.random.standard_normal((nParticles, 3))
+particlev = np.zeros_like(particle)
 
 
-@vectorize(['float32(float32, float32)'], target='cuda')
-def Add(a, b):
-    return a + b
+# @vectorize(['float32(float32, float32)'], target='cuda')
+# @njit(parallel=True, fastmath=True)
+# def nbody_nb(particle, particlev):  # NumPy arrays as input
+#     nSteps = 5
+#     dt = 0.01
+#     for step in range(1, nSteps + 1, 1):
+#         for i in range(nParticles):
+#             Fx = 0.0
+#             Fy = 0.0
+#             Fz = 0.0
+#             for j in range(nParticles):
+#                 if j != i:
+#                     dx = particle[j, 0] - particle[i, 0]
+#                     dy = particle[j, 1] - particle[i, 1]
+#                     dz = particle[j, 2] - particle[i, 2]
+#                     drSquared = dx * dx + dy * dy + dz * dz
+#                     drPowerN32 = 1.0 / (drSquared + sqrt(drSquared))
+#                     Fx += dx * drPowerN32
+#                     Fy += dy * drPowerN32
+#                     Fz += dz * drPowerN32
+#                 particlev[i, 0] += dt * Fx
+#                 particlev[i, 1] += dt * Fy
+#                 particlev[i, 2] += dt * Fz
+#         for i in range(nParticles):
+#             particle[i, 0] += particlev[i, 0] * dt
+#             particle[i, 1] += particlev[i, 1] * dt
+#             particle[i, 2] += particlev[i, 2] * dt
+
+@njit(parallel=True, fastmath=True)
+def nbody_nb(particle, particlev):  # NumPy arrays as input
+    dt = 0.01
+    for i in prange(nParticles):
+        Fx = 0.0
+        Fy = 0.0
+        Fz = 0.0
+        for j in range(nParticles):
+            if j != i:
+                dx = particle[j, 0] - particle[i, 0]
+                dy = particle[j, 1] - particle[i, 1]
+                dz = particle[j, 2] - particle[i, 2]
+                drSquared = dx * dx + dy * dy + dz * dz
+                drPowerN32 = 1.0 / (drSquared + sqrt(drSquared))
+                Fx += dx * drPowerN32
+                Fy += dy * drPowerN32
+                Fz += dz * drPowerN32
+            particlev[i, 0] += dt * Fx
+            particlev[i, 1] += dt * Fy
+            particlev[i, 2] += dt * Fz
+    for i in prange(nParticles):
+        particle[i, 0] += particlev[i, 0] * dt
+        particle[i, 1] += particlev[i, 1] * dt
+        particle[i, 2] += particlev[i, 2] * dt
 
 
-# Initialize arrays
-N = 100000
-A = np.ones(N, dtype=np.float32)
-B = np.ones(A.shape, dtype=A.dtype)
-C = np.empty_like(A, dtype=A.dtype)
+nbody_nb(particle, particlev)
+t0 = time.time()
+# nbody_nb(particle, particlev)
+nSteps = 5
+for i in range(1, nSteps + 1, 1):
+    nbody_nb(particle, particlev)
+ti_nb = time.time() - t0
 
-# Add arrays on GPU
-C = Add(A, B)
+print(ti_nb)
